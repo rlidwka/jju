d646c0fb8bbc873d1a0d24f31119fa88ed735fdf
Alex Kocharin <alex@kocharin.ru>
2013-11-12 10:41:21 +0400

    Document draft

---
+++ test/test_document.js
@@ -0,0 +1,12 @@
+// not yet developed
+
+/*var assert = require('assert')
+var create = require('../lib/document').Document
+
+assert.equal(create('"test"').get(''), 'test')
+assert.equal(create('"test"').get([]), 'test')
+assert.equal(create('"test"').get(false), 'test')
+assert.equal(create(undefined).get(''), undefined)
+
+//assert.equal(create('"test"').set('', 'foo').toString(), '"foo"')
+*/
