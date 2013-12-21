ce4c6fff178cba482f8064dcdca1be4f66ee24be
Alex Kocharin <alex@kocharin.ru>
2013-12-21 17:43:41 +0400

    lazily require files to speed up boot time

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-module.exports.parse = require('./lib/parse').parse
-module.exports.stringify = require('./lib/stringify').stringify
-module.exports.tokenize = require('./lib/parse').tokenize
-module.exports.update = require('./lib/document').update
+
+module.exports.__defineGetter__('parse', function() {
+	return require('./lib/parse').parse
+})
+
+module.exports.__defineGetter__('stringify', function() {
+	return require('./lib/stringify').stringify
+})
+
+module.exports.__defineGetter__('tokenize', function() {
+	return require('./lib/parse').tokenize
+})
+
+module.exports.__defineGetter__('update', function() {
+	return require('./lib/document').update
+})
+
